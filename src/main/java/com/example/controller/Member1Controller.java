e8b62a1a2799fcadd4f90f6e7b896a6ced751ea5
탁승은 <xkrtmddms@gmail.com>
2023-05-08 10:12:37 +0900

    20230508

--- src/main/java/com/example/controller/Member1Controller.java
+++ src/main/java/com/example/controller/Member1Controller.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.entity.Member1;
 import com.example.repository.Member1Repository;
@@ -36,12 +39,26 @@ public class Member1Controller {
         log.info(format, member1.toString()); //꼭 찍어보자
 
         //저장소 호출해서 DB에 넣기해야함
-        m1Repository.save(member1);
+        m1Repository.save(member1); //sql문을 안쓰면 DB종류 상관없이 넘어가니깐 이런점에선 좋음
         
         return "redirect:/member1/join.do";
     }
 
+
+/* ----------------------------------------------------------------- */
+
+    //회원전체조회
+    //ModelAndView란  model.Attribute + return html 혼합된거라고 생각하면 됨
+    @GetMapping(value = "selectlist.do")
+    public ModelAndView selectListGET(){ // ModelAndView를 쓰면 (Model model) 이렇게 하던거 안한다고 생각하면됨 
+        List<Member1> list = m1Repository.findAll();
+        return new ModelAndView("/member1/selectlist", "list", list); //(return 값, model.attribute 했던 값들 두 개)
+
+    }
+
     
+
+
 
 /* ----------------------------------------------------------------- */
     
