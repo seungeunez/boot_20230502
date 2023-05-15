ae2770969fa922933bf326840752ee0474f4d440
탁승은 <xkrtmddms@gmail.com>
2023-05-15 09:01:49 +0900

    20230515

---
+++ src/main/java/com/example/controller/jpa/Student2Controller.java
@@ -0,0 +1,15 @@
+package com.example.controller.jpa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping(value = "/student2")
+@RequiredArgsConstructor
+@Slf4j
+public class Student2Controller {
+    
+}
