60555fef2718c75d616c3e041333861c22fef73d
탁승은 <xkrtmddms@gmail.com>
2023-05-19 08:22:29 +0900

    Update Address1Repository.java

--- src/main/java/com/example/repository/Address1Repository.java
+++ src/main/java/com/example/repository/Address1Repository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Address1;
-import com.example.entity.Address1Projection;
 
 
 
